2cf4f242eb4cbbb6873c8ef8635d26843a8cdde1
jhonnoriega1221 <jhonlex17@gmail.com>
2022-04-24 20:52:51 -0500

    fix: some code was organizated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,42 +1,48 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+//Componentes
 import { AppToolbarComponent } from './components/app-toolbar/app-toolbar.component';
+import { AppToolbarSearchBarComponent } from './components/app-toolbar-search-bar/app-toolbar-search-bar.component';
 import { AppNavbarComponent } from './components/app-navbar/app-navbar.component';
 import { AppNavbarButtonComponent } from './components/app-navbar-button/app-navbar-button.component';
-import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
-
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip'; 
-import { MatRippleModule } from '@angular/material/core';
-import { MatButtonModule } from '@angular/material/button';
-import { AppToolbarSearchBarComponent } from './components/app-toolbar-search-bar/app-toolbar-search-bar.component';
 import { BackTitleButtonComponent } from './components/back-title-button/back-title-button.component';
 import { ListItemComponent } from './components/list-item/list-item.component';
+
+//Layouts
+import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
+
+//Modulos de Material Angular
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatRippleModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+
 
 
 
 @NgModule({
-  declarations: [
-    AppToolbarComponent,
-    AppNavbarComponent,
-    AppNavbarButtonComponent,
-    AppLayoutComponent,
-    AppToolbarSearchBarComponent,
-    BackTitleButtonComponent,
-    ListItemComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    MatButtonModule,
-    MatTooltipModule,
-    MatIconModule,
-    MatRippleModule
-  ],
-  exports: [
-    BackTitleButtonComponent,
-    ListItemComponent
-  ]
+    declarations: [
+        AppToolbarComponent,
+        AppToolbarSearchBarComponent,
+        AppNavbarComponent,
+        AppNavbarButtonComponent,
+        BackTitleButtonComponent,
+        ListItemComponent,
+        AppLayoutComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        MatButtonModule,
+        MatTooltipModule,
+        MatIconModule,
+        MatRippleModule
+    ],
+    exports: [
+        BackTitleButtonComponent,
+        ListItemComponent
+    ]
 })
-export class SharedModule { }
+export class SharedModule {}
--- src/app/tourist-products/tourist-products.module.ts
+++ src/app/tourist-products/tourist-products.module.ts
@@ -1,39 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TouristProductsRoutingModule } from './tourist-products-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+//Páginas
 import { ProductsListPageComponent } from './pages/products-list-page/products-list-page.component';
 import { ProductDetailsPageComponent } from './pages/product-details-page/product-details-page.component';
+
+//Componentes
 import { ProductsListComponent } from './components/products-list/products-list.component';
-import { SharedModule } from '../shared/shared.module';
 import { ProductInfoComponent } from './components/product-info/product-info.component';
 import { ProductAltPricesButtonComponent } from './components/product-alt-prices-button/product-alt-prices-button.component';
 import { ProductAltPricesListComponent } from './components/product-alt-prices-list/product-alt-prices-list.component';
+import { ProductPriceDialogComponent } from './components/product-price-dialog/product-price-dialog.component';
 
+//Modulos de Material Angular
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
-import { ProductPriceDialogComponent } from './components/product-price-dialog/product-price-dialog.component';
-
 @NgModule({
-  declarations: [
-    ProductsListPageComponent,
-    ProductDetailsPageComponent,
-    ProductsListComponent,
-    ProductInfoComponent,
-    ProductAltPricesButtonComponent,
-    ProductAltPricesListComponent,
-    ProductPriceDialogComponent,
-  ],
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    TouristProductsRoutingModule,
-    SharedModule
-  ]
+    declarations: [
+        ProductsListPageComponent,
+        ProductDetailsPageComponent,
+        ProductsListComponent,
+        ProductInfoComponent,
+        ProductAltPricesButtonComponent,
+        ProductAltPricesListComponent,
+        ProductPriceDialogComponent,
+    ],
+    imports: [
+        CommonModule,
+        TouristProductsRoutingModule,
+        SharedModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatInputModule
+    ]
 })
 export class TouristProductsModule { }
