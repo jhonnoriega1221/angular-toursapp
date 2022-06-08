f0888303501094332344f0dc90942bc9175fddf6
jhonnoriega1221 <jhonlex17@gmail.com>
2022-06-08 13:14:12 -0500

    fix: error message loading museum details fixed

--- src/app/museum/pages/museum-details-page/museum-details-page.component.ts
+++ src/app/museum/pages/museum-details-page/museum-details-page.component.ts
@@ -13,8 +13,8 @@ import { Exhibition } from '../../models/exhibition';
 export class MuseumDetailsPageComponent implements OnInit {
 
   private museumId: number = 0;
-  public isLoading: boolean = true;
-  public isError: boolean = false;
+  public isLoading: boolean[] = [true,true];
+  public isError: boolean[] = [false, false];
   public tempDate:string='Todos los días - 8:00am a 5:00pm';
   public loremDescription:string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat quis neque venenatis ullamcorper. Nam ultrices sapien vitae felis imperdiet, non lacinia libero tempus. Sed lacus diam, egestas ac est vitae, vulputate efficitur massa. Maecenas ut sodales nunc. Integer volutpat lectus in sem sollicitudin rutrum nec vitae nulla.'
 
@@ -39,27 +39,35 @@ export class MuseumDetailsPageComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
       this.museumId = params['id'];
-      this.getMuseum();
-      this.getExhibitions()
+      this.getMuseumData();
     })
   }
 
-  public getMuseum(): void {
+  public getMuseumData():void {
+    this.getMuseum();
+    this.getExhibitions();
+  }
 
-    this.isError = false;
+  private getMuseum(): void {
+
+    this.isError[0] = false;
+    this.isLoading[0] = true;
     this.museumService.getMuseum(this.museumId)
       .subscribe({
         next: (v) => this.museum = v,
         error: (e) => {
-          this.isError = true;
+          this.isError[0] = true;
+          this.isLoading[0] = false;
           console.error(e);
         },
-        complete: () => this.isLoading = false
+        complete: () => this.isLoading[0] = false
       });
       
   }
 
-  public getExhibitions(): void{
+  private getExhibitions(): void{
+    this.isError[1] = false;
+    this.isLoading[1] = true;
     this.exhibitionService.getMuseumExhibitions(this.museumId)
     .subscribe({
       next: (v) => {
@@ -68,10 +76,11 @@ export class MuseumDetailsPageComponent implements OnInit {
         });
       },
       error: (e) => {
-        this.isError = true;
+        this.isError[1] = true;
+        this.isLoading[1] = false;
         console.log(e);
       },
-      complete: () => this.isLoading = false
+      complete: () => this.isLoading[1] = false
     });
 
     
