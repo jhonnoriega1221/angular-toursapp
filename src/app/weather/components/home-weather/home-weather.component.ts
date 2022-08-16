791bc7927449577f68f9775b8aa51e44dd9befb7
jhonnoriega1221 <jhonlex17@gmail.com>
2022-08-16 10:23:45 -0500

    feat: weather aded

--- src/app/weather/components/home-weather/home-weather.component.ts
+++ src/app/weather/components/home-weather/home-weather.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition, state } from '@angular/animations';
+
 
 @Component({
   selector: 'app-home-weather',
   templateUrl: './home-weather.component.html',
-  styleUrls: ['./home-weather.component.scss']
+  styleUrls: ['./home-weather.component.scss'],
+  animations: [
+    trigger('showUpdateDate', [
+      state('visible', style({
+        'opacity': 1,
+      })),
+      transition('invisible => visible', animate(800))
+    ])
+  ]
 })
 export class HomeWeatherComponent implements OnInit {
+
+  public updateDateState: string = 'invisible';
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.updateDateState = 'visible';
+
   }
 
 }
